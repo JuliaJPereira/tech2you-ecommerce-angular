e62d5224ef4ae1d254b694761568d2e7318a2bae
Julia Pereira <juliapereira.odc@gmail.com>
2022-05-07 14:45:20 -0300

    Consumindo API carrinho

--- src/app/components/carrinho/carrinho.component.ts
+++ src/app/components/carrinho/carrinho.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Produtos } from 'src/app/models/produtos';
 
 @Component({
   selector: 'app-carrinho',
@@ -6,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carrinho.component.scss']
 })
 export class CarrinhoComponent implements OnInit {
+
+  carrinhoProdutos: Produtos[] = [
+    {
+      "id": 0,
+      "name": "Product Number 0",
+      "image": "//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6747399",
+      "oldPrice": 335,
+      "price": 317,
+      "description": "Product long description number 0 just to make more than one like of text.",
+    },
+    {
+      "id": 1,
+      "name": "Product Number 1",
+      "image": "//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6670538",
+      "oldPrice": 138,
+      "price": 111,
+      "description": "Product long description number 1 just to make more than one like of text.",
+    }
+  ]
 
   constructor() { }
 
